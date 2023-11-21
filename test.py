b59a90d5b24dd7994fc88afca5f8eb03edfa0fb5
Obidokun Tunji <tunjiobidokun@gmail.com>
2023-11-21 12:28:53 -0800

    Updates

--- test.py
+++ test.py
@@ -1,21 +1,4 @@
-# import os 
-# import openai
-# openai.api_key  = 
-# answer = openai.completion.create(
-#     model = "text-davinci-003",
-#     prompt = "Say this is a test",
-#     max_tokens = 7,
-#     temperature = 0
-# )
-
 import os
-from dotenv import load_dotenv
+import openai
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Access the API key
-api_key = os.getenv("API_KEY")
-
-# Now you can use the api_key variable in your code
-print(api_key)
+openai.api_key = os.getenv("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")
